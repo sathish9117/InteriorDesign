e6e888ed9ffcba7af9916b79ccc21937acbeb4c1
Sathish Kumar N <162999084+sathish9117@users.noreply.github.com>
2024-10-21 23:05:25 +0530

    Add Navbar component to App and wrap content in main element

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className=" overflow-x-hidden">
+      <Navbar />
+    </main>
+  );
 }
 
 export default App;
