fdd590c11df855a3d75b818067cb04eebe737b0d
Sathish Kumar N <162999084+sathish9117@users.noreply.github.com>
2024-10-22 22:55:55 +0530

    Hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import HeroPng from "../../assets/hero.png";
+const Hero = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2 md:min-h-[500px] gap-10">
+          {/* Text section */}
+          <div className="flex flex-col text-center justify-center gap-5 md:pr-8 md:text-left pt-20 md:pt-0 lg:pr-14">
+            <h1 className=" text-3xl font-bold">
+              SKETCH LUXURY INTERIOR DESIGN
+            </h1>
+            <p className=" text-sm text-gray-500">
+              Bring your dream to life with one-on-one design help & hand picked
+              products tailored to your style, space and budget.
+            </p>
+            <div className=" space-x-4">
+              <button className=" primary-btn uppercase bg-black text-white shadow-[5px_5px_0px_0px_#6c6c6c] hover:bg-white hover:text-black">
+                Get Started
+              </button>
+              <button className=" primary-btn uppercase">Contact Us</button>
+            </div>
+          </div>
+          {/* Images section */}
+          <div className="flex flex-col justify-center items-center">
+            <img
+              src={HeroPng}
+              alt=""
+              className=" w-[80%] md:w-[300px] object-cover md:scale-125"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
