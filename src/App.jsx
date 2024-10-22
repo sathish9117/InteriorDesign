7044af195b37d6ef3fc0d16679c1d48f1d915595
Sathish Kumar N <162999084+sathish9117@users.noreply.github.com>
2024-10-22 22:55:35 +0530

    add hero and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <main className=" overflow-x-hidden">
       <Navbar />
+      <Hero />
     </main>
   );
 }
