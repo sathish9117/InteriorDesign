49db22e3a113273170d8f971d5f1ea0663fb88e7
Sathish Kumar N <162999084+sathish9117@users.noreply.github.com>
2024-10-23 00:55:37 +0530

    remove border

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -7,13 +7,13 @@ import Brand5 from "../../assets/brand/5.png";
 const Brands = () => {
   return (
     <>
-      <div className="container py-14 border border-black">
-        <div className=" flex flex-wrap justify-center lg:justify-between gap-6">
-          <img src={Brand1} alt="brand" className=" w-[110px]" />
-          <img src={Brand2} alt="brand" className=" w-[110px]" />
-          <img src={Brand3} alt="brand" className=" w-[110px]" />
-          <img src={Brand4} alt="brand" className=" w-[110px]" />
-          <img src={Brand5} alt="brand" className=" w-[110px]" />
+      <div className="container py-14">
+        <div className=" flex flex-wrap border border-black justify-center lg:justify-evenly gap-6">
+          <img src={Brand1} alt="brand" className=" w-[130px]" />
+          <img src={Brand2} alt="brand" className=" w-[130px]" />
+          <img src={Brand3} alt="brand" className=" w-[130px]" />
+          <img src={Brand4} alt="brand" className=" w-[130px]" />
+          <img src={Brand5} alt="brand" className=" w-[130px]" />
         </div>
       </div>
     </>
